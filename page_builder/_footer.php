b88e9acd5cce8d6915915c526188bb1504cd0ce0
LetiziaLongoni <81821167+LetiziaLongoni@users.noreply.github.com>
2022-02-17 08:53:50 +0100

    Add files via upload

---
+++ page_builder/_footer.php
@@ -0,0 +1,7 @@
+<?php
+echo '<footer>
+        <div class="footer" align="center">
+          <h6 style="font-size: 14px; margin: 2px">Stage 2020 - 2021' .' Filippazzi Lorenzo - Longoni Letizia</h6>
+        </div>
+      </footer>'
+ ?>
---
+++ page_builder/_header.php
@@ -0,0 +1,24 @@
+<link rel="icon" href="<?=PREPATH?>assets/img/icon.png" type="image/png">
+
+<link rel="stylesheet" href="<?=PREPATH?>assets/plugin/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="<?=PREPATH?>assets/css/bootstrap-switch/bootstrap-switch.css">
+<!-- <link rel="stylesheet" href="<?=PREPATH?>assets/plugin/bootstrap/css/bootstrap-theme.min.css"> -->
+
+<link rel="stylesheet" href="<?=PREPATH?>assets/css/master.css">
+<link rel="stylesheet" href="<?=PREPATH?>assets/plugin/font-awesome/css/font-awesome.css">
+<script src="<?=PREPATH?>assets/plugin/fontawesome-free-5.0.9/svg-with-js/js/fontawesome-all.js"></script>
+
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+<script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+
+<script type="text/javascript" src="<?=PREPATH?>assets/plugin/bootstrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="<?=PREPATH?>assets/plugin/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="<?=PREPATH?>assets/css/bootstrap-switch/bootstrap-switch.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.2/bootstrap-slider.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.2/css/bootstrap-slider.css" />
+
+
